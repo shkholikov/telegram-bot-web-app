ca3ecf39a686e505db60240ee77ab65d5018857c
shkholikov <imshakhzod@gmail.com>
2023-06-20 01:00:09 +0200

    Fix server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 import { webhookCallback } from "./deno.deps.ts"
 import { bot } from "./bot.ts";
 
